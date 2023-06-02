c0c2a951bc4861438f007e5adbdb1c099792ea65
wbadmin <>
2023-06-02 14:04:17 +0200

    {/src/main/java/com/myspace/folt/Customer.java}

--- src/main/java/com/myspace/folt/Customer.java
+++ src/main/java/com/myspace/folt/Customer.java
@@ -6,12 +6,44 @@ package com.myspace.folt;
 
 public class Customer implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Customer() {
-    }
+	private java.lang.String email;
+	private java.lang.String name;
+	private java.lang.String address;
 
+	public Customer() {
+	}
 
+	public java.lang.String getEmail() {
+		return this.email;
+	}
 
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(java.lang.String address) {
+		this.address = address;
+	}
+
+	public Customer(java.lang.String email, java.lang.String name,
+			java.lang.String address) {
+		this.email = email;
+		this.name = name;
+		this.address = address;
+	}
 
 }
